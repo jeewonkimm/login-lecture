fb9f9bcb95ea39a73d81e3df548f1f10ea9c1d9e
Jeewon <wldnjsqnsl@gmail.com>
2022-02-13 13:40:55 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,16 +1,28 @@
-"use sstrict";
+"use strict";
 
 const id = document.querySelector("#id"),
     psword = document.querySelector("#psword"),
     loginBtn = document.querySelector("button");
 
 
+
 loginBtn.addEventListener("click",login);
+
 
 function login(){
     const req = {
-        id:id.value,
-        psword:psword.value,
+        id: id.value,
+        psword: psword.value,
+
     };
-    console.log(req);
+  
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    });
+
 }

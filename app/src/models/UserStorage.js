71f8a60c0e3afca3fa61a9459aad1fec72abf360
Jeewon <wldnjsqnsl@gmail.com>
2022-02-14 18:32:58 +0900

    UserStorage 모델 구현
    Signed-off-by: Jeewon <wldnjsqnsl@gmail.com>

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,25 @@
+"user strict";
+
+const { get } = require("../routes/home");
+
+class UserStorage{
+    static #users = {
+        id: ["woorimIT", "나개발", "김팀장"],
+        psword: ["1234", "1234", "123456"],
+        nams: ["우리밋","나개발","김팀장"],
+    };
+
+    static getUsers(...fields){
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field)=>{
+            if(users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        },{});
+    
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -25,15 +25,15 @@ function login(){
         body: JSON.stringify(req),
     })
     .then((res)=> res.json())
-    .then((res)=>{
+    .then((res) => {
         if(res.success){
-            location.href="/";
+            location.href ="/";
         }else{
             alert(res.msg);
         }
     })
     .catch((err)=>{
-        console.error(new Error("로그인 중 에러 발생"));
+        console.error(new Error("존재하지 않는 아이디,비번"));
     });
 
 }
